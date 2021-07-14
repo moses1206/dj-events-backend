4e63141f4d13ddc42a724868c5a99c666e322870
moses1206 <mose23@naver.com>
2021-07-14 21:52:52 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef9fa9a67068666948ea75919bf89b13'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c402726f-6275-43f3-81a0-205891a26d52'
+};
